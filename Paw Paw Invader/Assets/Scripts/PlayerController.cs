1798c5f51e550aa1f16c9383356432a6edd1d4c2
ropok <jalerse@gmail.com>
2022-09-04 21:19:12 +0700

    Initiate Player Movement
    - Horizontal input to move left and right
    - Set boundary to prevent moving out of the screen

---
+++ Paw Paw Invader/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    [SerializeField] private float thrust = 20.0f;
+
+    // boundary
+    private float boundaryX = 8.0f;
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovePlayer(thrust);
+        MovementBoundary(boundaryX);
+
+    }
+
+    void MovePlayer(float thrust)
+    {
+        float horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime;
+        rb2D.MovePosition(rb2D.position + new Vector2(horizontalInput * thrust, 0));
+    }
+
+    void MovementBoundary(float boundaryX)
+    {
+        if (transform.position.x >= boundaryX)
+        {
+            transform.position = new Vector2(boundaryX, transform.position.y);
+        }
+        if (transform.position.x <= -boundaryX)
+        {
+            transform.position = new Vector2(-boundaryX, transform.position.y);
+        }
+    }
+
+}
