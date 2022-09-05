192ea0710870b747b5df0567622efd414c2aed4c
ropok <jalerse@gmail.com>
2022-09-05 20:30:42 +0700

    Cat Controller movement
    - Add random movement to Cat, move left and right

---
+++ Paw Paw Invader/Assets/Scripts/CatController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    [SerializeField] float thrust = 1.0f;
+    [SerializeField] GameObject projectilePrefab;
+
+    // boundary
+    private float boundaryX = 8.0f;
+    private float boundaryMove;
+    private float currentPosition;
+
+    private bool isMoving = false;
+    private float movingDirection = 1;
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovePlayer(thrust);
+        MovementBoundary(boundaryX);
+        ShootControl(projectilePrefab);
+
+    }
+
+    void MovePlayer(float thrust)
+    {
+        if (transform.position.x >= currentPosition + boundaryMove || transform.position.x <= currentPosition - boundaryMove)
+        {
+            isMoving = false;
+        }
+
+        if (isMoving)
+        {
+            rb2D.MovePosition(rb2D.position + new Vector2(movingDirection * thrust * Time.deltaTime, 0));
+        }
+        else
+        {
+            GetBoundaryMovement(boundaryX/2);
+            currentPosition = transform.position.x;
+            movingDirection = -movingDirection;
+            isMoving = true;
+        }
+    }
+
+    void MovementBoundary(float boundaryX)
+    {
+        if (transform.position.x >= boundaryX)
+        {
+            transform.position = new Vector2(boundaryX, transform.position.y);
+            isMoving = false;
+        }
+        if (transform.position.x <= -boundaryX)
+        {
+            transform.position = new Vector2(-boundaryX, transform.position.y);
+            isMoving = false;
+        }
+    }
+
+    void ShootControl(GameObject projectilePrefab)
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        //}
+    }
+
+    float GetBoundaryMovement(float boundary_X)
+    {
+        boundaryMove = Random.Range(0, boundary_X);
+        return boundaryMove;
+    }
+
+}
